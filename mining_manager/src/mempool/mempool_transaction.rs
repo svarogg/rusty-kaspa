e1766c39701d3b8a2072dfcdda065b7208b9efb4
Mike Zak <feanorr@gmail.com>
2022-09-27 14:03:08 +0300

    Fix stuff

--- mining_manager/src/mempool/mempool_transaction.rs
+++ mining_manager/src/mempool/mempool_transaction.rs
@@ -1,17 +1,18 @@
 use consensus_core::tx::{Transaction, TransactionId};
 use std::collections::HashMap;
+use std::sync::Arc;
 
 #[derive(Clone)]
-pub(crate) struct MempoolTransaction<'a> {
-    pub(crate) transaction: &'a Transaction,
-    pub(crate) parent_transactions_in_pool: &'a HashMap<TransactionId, MempoolTransaction<'a>>,
+pub(crate) struct MempoolTransaction {
+    pub(crate) transaction: Arc<Transaction>,
+    pub(crate) parent_transactions_in_pool: Arc<HashMap<TransactionId, MempoolTransaction>>,
     pub(crate) is_high_priority: bool,
     pub(crate) added_at_daa_score: u64,
 }
 
-impl<'a> MempoolTransaction<'a> {
+impl MempoolTransaction {
     pub fn new(
-        transaction: &'a Transaction, parent_transactions_in_pool: &'a HashMap<TransactionId, MempoolTransaction<'a>>,
+        transaction: Arc<Transaction>, parent_transactions_in_pool: Arc<HashMap<TransactionId, MempoolTransaction>>,
         is_high_priority: bool, added_at_daa_score: u64,
     ) -> Self {
         Self { transaction, parent_transactions_in_pool, is_high_priority, added_at_daa_score }
--- mining_manager/src/mempool/transactions_pool.rs
+++ mining_manager/src/mempool/transactions_pool.rs
@@ -3,11 +3,12 @@ use crate::mempool::transactions_ordered_by_fee::TransactionsOrderedByFee;
 use crate::mempool::Mempool;
 use consensus_core::tx::{Transaction, TransactionId};
 use std::collections::HashMap;
+use std::sync::Arc;
 
 pub(crate) struct TransactionsPool<'a> {
-    all_transactions: HashMap<TransactionId, &'a MempoolTransaction<'a>>,
-    high_priority_transactions: HashMap<TransactionId, &'a MempoolTransaction<'a>>,
-    chained_transactions_by_parent_id: HashMap<TransactionId, Vec<&'a MempoolTransaction<'a>>>,
+    all_transactions: HashMap<TransactionId, MempoolTransaction<'a>>,
+    high_priority_transactions: HashMap<TransactionId, MempoolTransaction<'a>>,
+    chained_transactions_by_parent_id: HashMap<TransactionId, Vec<MempoolTransaction<'a>>>,
     transactions_ordered_by_fee: TransactionsOrderedByFee<'a>,
 }
 
@@ -28,17 +29,18 @@ impl<'a> TransactionsPool<'a> {
 }
 
 impl<'a> TransactionsPool<'a> {
-    pub(crate) fn add_transaction_to_transactions_pool(
-        &mut self, mempool: &Mempool, transaction: &'a Transaction,
-        parent_transactions_in_pool: &'a HashMap<TransactionId, MempoolTransaction<'a>>, is_high_priority: bool,
-    ) -> MempoolTransaction<'a> {
+    pub(crate) fn add_transaction(
+        &'a mut self, mempool: &Mempool, transaction: Arc<Transaction>,
+        parent_transactions_in_pool: Arc<HashMap<TransactionId, MempoolTransaction>>, is_high_priority: bool,
+    ) -> MempoolTransaction {
         let virtual_daa_score = mempool.consensus.virtual_daa_score();
-        let mempool_transaction: MempoolTransaction<'a> =
-            MempoolTransaction::new(transaction, parent_transactions_in_pool, is_high_priority, virtual_daa_score);
         let transaction_id = transaction.id();
 
+        let mempool_transaction: MempoolTransaction =
+            MempoolTransaction::new(transaction, parent_transactions_in_pool, is_high_priority, virtual_daa_score);
+
         self.all_transactions
-            .insert(transaction_id, &mempool_transaction);
+            .insert(transaction_id, mempool_transaction.clone());
 
         for (parent_transaction_id, parent_transaction) in parent_transactions_in_pool {
             let entry = self
@@ -46,7 +48,7 @@ impl<'a> TransactionsPool<'a> {
                 .entry(*parent_transaction_id)
                 .or_insert(Vec::new());
 
-            entry.push(&mempool_transaction)
+            entry.push(mempool_transaction.clone())
         }
 
         mempool_transaction
